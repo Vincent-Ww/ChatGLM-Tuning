bb9adc3cc21d7dec1525ffaba19a3f2ae8350d06
xiezizhe <xiezizhe@kuaishou.com>
2023-05-27 01:24:03 +0800

    诉求llm

--- llm_ft_evaluete_MRecog_onAI(一天全量未筛选).py
+++ llm_ft_evaluete_MRecog_onAI(一天全量未筛选).py
@@ -14,9 +14,12 @@ import pandas as pd
 import traceback
 from q_matching import Retrieval
 
-PEFT_PATH = "/home/xiezizhe/wuzixun/LLM/ChatGLM-Tuning/output-question-20230520/"
-CHATGLM_PATH = "/home/xiezizhe/wuzixun/LLM/chatglm-6b"
-DEV_DATA_PATH = "/home/xiezizhe/wuzixun/LLM/ChatGLM-Tuning/data/ks_ai_ft_eval/ai_ft_0524_一天全量_format.json"
+#PEFT_PATH = "/home/xiezizhe/wuzixun/LLM/ChatGLM-Tuning/output-question-20230520/"
+PEFT_PATH = "/home/xiezizhe/wuzixun/LLM/ChatGLM-Tuning/aidp-dialogue-intent-v2"
+#CHATGLM_PATH = "/home/xiezizhe/wuzixun/LLM/chatglm-6b"
+CHATGLM_PATH = "/home/xiezizhe/hanzhou/chatglm-6b"
+#DEV_DATA_PATH = "/home/xiezizhe/wuzixun/LLM/ChatGLM-Tuning/data/ks_ai_ft_eval/ai_ft_0524_一天全量_format.json"
+DEV_DATA_PATH = "/home/xiezizhe/wuzixun/LLM/ChatGLM-Tuning/data/ks_ai_ft_eval/ai_ft_0524_一天全量_5原因_format.json"
 
 
 def chatglm_inference(model, tokenizer, sample):
@@ -103,9 +106,9 @@ if __name__ == "__main__":
 
             nrow += 1
             if nrow % 200 == 0 and nrow != 0:
-                workbook.save(f"智能对话LLM验证{nrow}_匹配人工Q_未筛选.xlsx")
-                print(f"智能对话LLM验证{nrow}_匹配人工Q_未筛选.xlsx: Save!")
+                workbook.save(f"诉求LLM验证{nrow}_匹配人工Q_未筛选.xlsx")
+                print(f"诉求LLM验证{nrow}_匹配人工Q_未筛选.xlsx: Save!")
         except Exception as e:
             traceback.print_exc()
             print("Exception: ", e)
-    workbook.save(f"智能对话LLM验证{nrow}_匹配人工Q_未筛选.xlsx")
+    workbook.save(f"诉求LLM验证{nrow}_匹配人工Q_未筛选.xlsx")
